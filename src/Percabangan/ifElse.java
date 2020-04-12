42555ee7abd1a46cb16b609e5fff953eaec1bd28
achmad habibi <habiby.ach@gmail.com>
2020-04-12 18:37:53 +0800

    condition 1 false, condition 2true

--- src/Percabangan/ifElse.java
+++ src/Percabangan/ifElse.java
@@ -2,13 +2,17 @@ package Percabangan;
 
 public class ifElse {
     public static void main(String[] args) {
-        double  totalbelaja = 25000;
-        double uangDidompet = 53000;
+        double  totalbelaja = 53000;
+        double uangDidompet = 25000;
 
-        if(uangDidompet<totalbelaja){
+        if(uangDidompet<totalbelaja) {
             System.out.println("Uang Kurang, kurangi jajan anda");
-        }else{
-            System.out.println("Uang cukup, Selamat belanja");
+        }else if (uangDidompet>totalbelaja){
+            double  angsul = uangDidompet-totalbelaja;
+            System.out.println("uang cukup, angsul  :" +angsul);
+        }
+        else{
+            System.out.println("Uang anda pas selamat");
         }
     }
 }
