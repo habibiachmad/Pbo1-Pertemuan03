4937c32841f296c03e935a769d183b33c8e593aa
achmad habibi <habiby.ach@gmail.com>
2020-04-12 18:23:24 +0800

    Operator Unary

--- src/Operator/OperationBinary.java
+++ src/Operator/OperationBinary.java
@@ -1,8 +1,18 @@
 package Operator;
 
+import java.sql.SQLOutput;
+
 public class OperationBinary {
     String nama = "Achmad Habibi";
     int JumlahSks = 24;
     double   indeksprestasikumulatif = 3.00;
 
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
