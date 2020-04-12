93f57afb044da534a33fce23e484853dcdf6152b
achmad habibi <habiby.ach@gmail.com>
2020-04-12 18:32:46 +0800

    condition true

---
+++ src/Percabangan/ifElse.java
@@ -0,0 +1,14 @@
+package Percabangan;
+
+public class ifElse {
+    public static void main(String[] args) {
+        double  totalbelaja = 53000;
+        double uangDidompet = 25000;
+
+        if(uangDidompet<totalbelaja){
+            System.out.println("Uang Kurang, kurangi jajan anda");
+        }else{
+            System.out.println("Uang cukup, Selamat belanja");
+        }
+    }
+}
