e1bb0e40ef15546ba9dc9a2d5b2803a81dbd14bd
achmad habibi <habiby.ach@gmail.com>
2020-04-12 18:41:30 +0800

    switch1

---
+++ src/Percabangan/Switch.java
@@ -0,0 +1,24 @@
+package Percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanjalan=3;
+        switch(jalanjalan){
+            case 1:
+                System.out.println("urang lapas");
+                break  ;
+            case 2;
+                System.out.println("urang acayan");
+                break;
+                case 3;
+                System.out.println("urang nyamuk");
+                break;
+                case 4;
+                    System.out.println("dubil dit");
+                    break;
+                    case 5;
+                        System.out.println("makhluk halus");
+                        break;
+        }
+    }
+}
